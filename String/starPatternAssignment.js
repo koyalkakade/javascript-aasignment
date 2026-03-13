fde244c9f5bdbd1f00e747d305c97b61f9a46a9c
Koyal Nimbalkar <koyalkakade@gmail.com>
2026-03-13 11:33:21 +0530

    update start pattern file

--- String/starPatternAssignment.js
+++ String/starPatternAssignment.js
@@ -86,6 +86,106 @@ function printReversePyramid(n) {
 
 printReversePyramid(5);
 
+//6. print number pyramid
+function printNumberPyramid(n) {
+    console.log('5. ---- print number Pyramid----');
+    for (let i = 1; i <= n; i++) {
+        let spaces = "";
+        for (let j = 1; j <= n - i; j++) {
+            spaces += " ";
+        }
+        let stars = "";
+        for (let k = 1; k <= (2 * i - 1); k++) {
+            stars += i;
+        }
+        //or
+        //  for (let k = 1; k <= i; k++) {
+        //     stars += i+" ";
+        // }
+        console.log(spaces + stars);
+    }
+}
+printNumberPyramid(5);
+
+//7. print pascals pyramid
+function printPascalsPyramid(n) {
+    console.log('7. ---- print pascals Pyramid----');
+    for (let i = 0; i < n; i++) {
+        let spaces = "";
+        for (let j = 1; j <= n - i; j++) {
+            spaces += " ";
+        }
+        let output = '';
+        for (let j = 0; j <= i; j++) {
+            output += pascalNumber(i, j) + ' ';
+        }
+        console.log(spaces + output);
+    }
+}
+
+function pascalNumber(row, column) {
+    if (column === 0 || column === row) {
+        return 1;
+    } else {
+        let res = pascalNumber(row - 1, column - 1);
+        let res1 = pascalNumber(row - 1, column);
+        // console.log('res-',res,'res1- ',res1);
+        return res + res1;
+        // return pascalNumber(row - 1, column - 1) +
+        //     pascalNumber(row - 1, column);
+    }
+}
+
+printPascalsPyramid(5);
+
+//8. Hollow Square
+console.log('8. ---- print Hollow Square----');
+function hollowSquarePattern(n) {
+    for (let i = 0; i <= n; i++) {
+        let stars = "";
+        for (j = 0; j <= n; j++) {
+            if (i == 0 || i == n ||j == 0 || j == n) {
+                stars += "* ";
+            }
+            else {
+                    stars += "  ";
+                }
+        }
+        console.log(stars);
+    }
+}
+
+hollowSquarePattern(5);
+
+//9. Hourglass Pattern
+function printHourglassPattern(n) {
+    console.log('9. ---- print Hourglass Pattern----');
+      for (let i = n; i >= 1; i--) {
+        let spaces = "";
+        for (let j = 1; j <= n - i; j++) {
+            spaces += " ";
+        }
+        let stars = "";
+        for (let k = 1; k <= (2 * i - 1); k++) {
+            stars += "*";
+        }
+        console.log(spaces + stars);
+    }
+    for (let i = 1; i <= n; i++) {
+        let spaces = "";
+        for (let j = 1; j <= n - i; j++) {
+            spaces += " ";
+        }
+        let stars = "";
+        for (let k = 1; k <= (2 * i - 1); k++) {
+            stars += "*";
+        }
+        console.log(spaces + stars);
+    }
+}
+
+printHourglassPattern(5)
+
 
 
 
